90423f264079e8495f9bb693bec0eff60db0fc26
Алексей Заикин <hydrombee@gmail.com>
2023-08-06 23:10:54 +0200

    Изменен стиль для редактирования

--- src/components/TaskEditForm.tsx
+++ src/components/TaskEditForm.tsx
@@ -1,60 +1,58 @@
+// TaskEditForm.tsx
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
 import { Task } from '../types'
-import { Button, TextField } from '@mui/material'
+import { Button, CardActions, CardContent, TextField, Typography } from '@mui/material'
 
-const TaskEditForm: React.FC<{
+interface TaskEditFormProps {
   task: Task
   onSaveTask: (task: Task) => void
   onCancelEdit: () => void
-}> = ({ task, onSaveTask, onCancelEdit }) => {
+}
+
+const TaskEditForm: React.FC<TaskEditFormProps> = ({ task, onSaveTask, onCancelEdit }) => {
   const [title, setTitle] = useState(task.title)
   const [description, setDescription] = useState(task.description)
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    onSaveTask({ ...task, title, description })
+    onSaveTask({ ...task, title, description }) // Создаем новый объект задачи с обновленными полями и передаем обратно в TaskItem
     onCancelEdit()
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        id='outlined-basic'
-        variant='outlined'
-        label='Название задачи'
-        placeholder='Введите название задачи'
-        onChange={(e) => setTitle(e.target.value)}
-      />
-
-      <TextField
-        id='outlined-basic'
-        variant='outlined'
-        label='Описание задачи'
-        placeholder='Введите описание задачи'
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-
-      <Button variant='outlined' type='submit'>
-        Сохранить
-      </Button>
-      <Button variant='outlined' type='button' onClick={onCancelEdit}>
-        Отменить
-      </Button>
-    </form>
+    <>
+      <CardContent style={{ display: 'flex', flexDirection: 'column' }}>
+        <TextField
+          style={{ marginBottom: '1rem' }}
+          id='outlined-basic'
+          variant='outlined'
+          label='Название задачи'
+          placeholder='Введите название задачи'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <TextField
+          style={{ marginBottom: '1rem' }}
+          id='outlined-basic'
+          variant='outlined'
+          label='Описание задачи'
+          placeholder='Введите описание задачи'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          multiline
+          rows={4}
+        />
+      </CardContent>
+      <CardActions>
+        <Button variant='outlined' type='button' onClick={handleSubmit}>
+          Сохранить
+        </Button>
+        <Button variant='outlined' type='button' onClick={onCancelEdit}>
+          Отменить
+        </Button>
+      </CardActions>
+    </>
   )
-}
-
-TaskEditForm.propTypes = {
-  task: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired,
-  }).isRequired,
-  onSaveTask: PropTypes.func.isRequired,
-  onCancelEdit: PropTypes.func.isRequired,
 }
 
 export default TaskEditForm
